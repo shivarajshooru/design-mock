c2551aa8c361f77aabca414c967f27180467a106
shivaraj shooru <shivarajshooru@gmail.com>
2021-10-26 20:48:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users,setUsers] = useState("");
+
+  useEffect(()=>{
+async function getData(){
+  let data = await fetch("https://jsonplaceholder.typicode.com/users")
+  .then((res)=>res.json())
+  setUsers([...data]);
+}
+getData();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(users)}
+      {users ? users.map((item,index)=>{
+        return <div key={index} className="user">
+          <img className="avatar" src="https://avatars.dicebear.com/v2/avataaars/%7B%7Busername%7D%7D.svg?options[mood][]=happy" alt="avatar" />
+          <hr/>
+      <p>Id: {  item.id}</p>
+      <p>Name: {  item.name}</p>
+      <p>UserName:{  item.username}</p>
+      <p>Website:{  item.website}</p>
+      <p>Phone:{  item.phone}</p>
+      <p>Email:{  item.email}</p>
+      {console.log(item.address)}
+      <div>Address:<p>City:{item.address["city"]}</p>
+      <div>geo:<p>lat:{item.address["geo"]["lat"]}</p>
+      <p>lng:{item.address["geo"]["lng"]}</p>
+      </div>
+      <p>Street:{item.address["street"]}</p>
+      <p>Suite:{item.address["suite"]}</p>
+      <p>Zipcode:{item.address["zipcode"]}</p>
+      </div>
+          </div>
+      }):null}
+      
     </div>
   );
 }
